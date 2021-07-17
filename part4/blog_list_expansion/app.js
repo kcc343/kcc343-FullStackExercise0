0fa5e0bffca05aa26bdd854d84f886ad68c51a18
kcc343 <kcc343@uw.edu>
2021-07-16 17:58:48 -0700

    ex 4.18, 4.19 added token-based authentication and designation of creator of blog

--- part4/blog_list_expansion/app.js
+++ part4/blog_list_expansion/app.js
@@ -6,6 +6,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const blogRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
@@ -22,6 +23,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- part4/blog_list_expansion/controllers/blogs.js
+++ part4/blog_list_expansion/controllers/blogs.js
@@ -1,7 +1,16 @@
+const jwt = require('jsonwebtoken')
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 require('express-async-errors')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
@@ -11,24 +20,30 @@ blogRouter.get('/', async (request, response) => {
   
 blogRouter.post('/', async (request, response) => {
   const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+
   if (!body.likes) {
     body.likes = 0
   }
   if (!body.title || !body.url) {
     response.status(400).end()
   } else {
-    const user = await User.find({});
     const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
         likes: body.likes,
-        user: user[0]._id
+        user: user._id
     })
     console.log(blog)
     const savedBlog = await blog.save()
-    user[0].blogs = user[0].blogs.concat(savedBlog._id)
-    await user[0].save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
 
     response.status(201)
     response.json(savedBlog.toJSON())
