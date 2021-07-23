59e007b77e87b902ce37607e97790540ea2dc074
kcc343 <kcc343@uw.edu>
2021-07-22 17:12:12 -0700

    ex 4.20 refactored getting token into middleware

--- part4/blog_list_expansion/controllers/blogs.js
+++ part4/blog_list_expansion/controllers/blogs.js
@@ -4,14 +4,6 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 require('express-async-errors')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
-
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
     .find({}).populate('user', { username: 1, name: 1 })
@@ -20,8 +12,7 @@ blogRouter.get('/', async (request, response) => {
   
 blogRouter.post('/', async (request, response) => {
   const body = request.body
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
--- part4/blog_list_expansion/utils/middleware.js
+++ part4/blog_list_expansion/utils/middleware.js
@@ -15,12 +15,25 @@ const unknownEndpoint = (request, response) => {
 const errorHandler = (error, request, response, next) => {
   if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token'
+    })
   }
   next(error)
+}
+
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  next()
 }
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
